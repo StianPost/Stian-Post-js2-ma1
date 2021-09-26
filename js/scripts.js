ba47523297926ce341d71ce9ae25610f7ad276a1
Stian <69841774+StianPost@users.noreply.github.com>
2021-09-26 18:39:24 +0200

    added better cards and loading gif

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ import { filteringAnArray } from './lib/filterAnArray.js';
 import renderCards from './components/renderCards.js';
 
 const api = await apiCall('https://fakestoreapi.com/products');
+const loading = document.querySelector('.loading');
 
 let prodCards = '';
 
 function cardHtml(params) {
+  loading.innerHTML = ``;
   params.forEach((element) => {
     prodCards += `
           <div class="col-4">
@@ -27,29 +29,25 @@ console.log(api);
 
 let search = document.querySelector('.search');
 let searchResults = document.querySelector('.prodContainer');
-let result = document.querySelector('.results');
 
 search.onkeyup = function () {
   searchResults.innerHTML = '';
-  if (search.value === '') {
-    searchResults.innerHTML = '';
-    return;
-  }
 
   let filteredArray = filteringAnArray(api, search.value);
 
   filteredArray.forEach((element) => {
     document.querySelector('.prodContainer').innerHTML += `
-          <div class="sm-col-2">
-              <div class="prodCard">
-                  <h1>${element.title}</h1>
-              </div>
-          </div>
+    <div class="col-4">
+    <div class="prodCard">
+      <img src="${element.image}" alt="Image of ${element.title}" class="prodCard__img"/>
+        <h1>${element.title}</h1>
+        <p>${element.price}</p>
+    </div>
+</div>
           `;
   });
-  console.log(filteredArray);
 
-  if (filteredArray === []) {
-    console.log('hello');
-  }
+  // if (filteredArray === []) {
+  //   console.log('hello');
+  // }
 };
